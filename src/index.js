9b32caf32c87e746f53451fc9de22afa1e48cb09
Annabelle Benedicti <abenedicti@outlook.fr>
2026-01-19 15:51:48 +0100

    Lab expense tracker done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description, paid);
+    this.type = "expense";
+    this.paid = paid;
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(newEntry) {
+    this.entries.push(newEntry);
+  }
+
+  getCurrentBalance() {
+    let total = 0;
+
+    for (let i = 0; i < this.entries.length; i++) {
+      let entry = this.entries[i];
+
+      if (entry.type === "income") {
+        total += entry.amount;
+      } else if (entry.type === "expense") {
+        total -= entry.amount;
+      }
+    }
+    return total;
+  }
+}
